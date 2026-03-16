4f78e174bd9f43984711b608f43bf328ae19993e
Vladislav Ukhlichev <uhlichevvladislav@yandex.ru>
2026-03-16 19:45:39 +0300

    1

---
+++ bot/keyboards/reply.py
@@ -0,0 +1,7 @@
+from aiogram.types import KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+def phone_keyboard():
+    builder = ReplyKeyboardBuilder()
+    builder.add(KeyboardButton(text="📱 Отправить номер", request_contact=True))
+    return builder.as_markup(resize_keyboard=True)
