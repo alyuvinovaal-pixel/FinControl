76cf5f5362cb7c6e09595484f4041bbec51403f6
Dmitry Mironov <dsemerikov2007@gmail.com>
2026-03-25 11:26:12 +0300

    Delete FinControl/bot directory
    Cleaning up the directory

--- FinControl/bot/keyboards/reply.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import KeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-
-def phone_keyboard():
-    builder = ReplyKeyboardBuilder()
-    builder.add(KeyboardButton(text="Отправить номер", request_contact=True))
-    return builder.as_markup(resize_keyboard=True)
