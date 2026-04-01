ba5914a92909a3c7186254dc6ac3fa244f698563
Dimiriko48 <dsemerikov2007@gmail.com>
2026-04-01 19:15:22 +0300

    Реализованы все контроллеры и убраны все прямые взаимодействия с БД

--- fincontrolapp/controllers/__init__.py
+++ fincontrolapp/controllers/__init__.py
@@ -1,11 +1,17 @@
-# from .auth_ctrl import AuthController
-# from .expenses_ctrl import ExpensesController
-from .goals_ctrl import GoalsController
 from .home_ctrl import HomeController
+from .goals_ctrl import GoalsController
 from .subscriptions_ctrl import SubscriptionsController
+from .transactions_ctrl import TransactionsController
+from .expenses_ctrl import ExpensesController
+from .income_ctrl import IncomeController
+from .settings_ctrl import SettingsController
 
 __all__ = [
-    "GoalsController",
     "HomeController",
+    "GoalsController",
     "SubscriptionsController",
+    "TransactionsController",
+    "ExpensesController",
+    "IncomeController",
+    "SettingsController",
 ]
